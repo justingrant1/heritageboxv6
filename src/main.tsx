0c9cf2cecf0f0d2232d48e9e310b08d17602904b
justingrant1 <jgrant@trestacapital.com>
2025-08-29 23:34:58 -0400

    revert 1

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import * as Sentry from "@sentry/react";
 import App from './App.tsx';
 import './index.css';
-
-Sentry.init({
-  dsn: "https://9c294a8d114361c4d14f915bac0d2435@o4509873209999360.ingest.us.sentry.io/4509873238573056",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-  integrations: [
-    Sentry.replayIntegration()
-  ],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
